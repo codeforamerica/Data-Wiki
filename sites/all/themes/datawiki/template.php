8932c1194ad1e51f48027ad104680e27409618c7
Chach Sikes <chachasikes@gmail.com>
2011-06-10 11:38:05 -0700

    Moved theme back into folder.

---
+++ sites/all/themes/datawiki/template.php
@@ -0,0 +1,17 @@
+<?php
+
+// Include preprocessing functions
+include('datawiki.preprocess.inc');
+
+/**
+ * Implements hook_form_alter().
+ */
+function datawiki_form_alter(&$form, &$form_state, $form_id) {
+  // Add appropriate title for search form
+  if ($form_id == 'search_block_form') {
+    $form['search_block_form']['#title'] = '';
+    unset($form['search_block_form']['#title_display']);
+    // HTML5 placeholder attribute
+    $form['search_block_form']['#attributes']['placeholder'] = t('SEARCH');
+  }
+}
