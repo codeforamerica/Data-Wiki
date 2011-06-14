16772666ac6ec0254fc71a4a9e2075a4f914c48a
Chach Sikes <chachasikes@gmail.com>
2011-06-14 09:32:16 -0700

    Added search box on map page.

--- sites/all/modules/custom/datawiki/datawiki_group_map/includes/map.html.inc.php
+++ sites/all/modules/custom/datawiki/datawiki_group_map/includes/map.html.inc.php
@@ -10,6 +10,9 @@
   <link rel="stylesheet" href="<?php print $html_root; ?>/includes/style/fluid_grid.css" type="text/css" />
   <link rel="stylesheet" href="<?php print $html_root; ?>/includes/style/boilerplate.css" type="text/css" />
   <link rel="stylesheet" href="<?php print $html_root; ?>/includes/js/leaflet/leaflet.css" />
+  <link rel="stylesheet" href="sites/all/themes/datawiki/css/style.css"  />
+
+  
   <link rel="stylesheet" href="<?php print $html_root; ?>/includes/style/style.css" type="text/css" />
 <!--[if lte IE 8]><link rel="stylesheet" href="<?php print $html_root; ?>/includes/style/leaflet/leaflet.ie.css" /><![endif]-->
   <script type="text/javascript" src="<?php print $html_root; ?>/includes/js/jquery-1.5.min.js"></script>
@@ -19,7 +22,9 @@
 <body>
 <div id="container" class="section container_12">
     <div id="header">
-      <div class="logo grid_6 alpha"><h1><a href="/">CityGroups: Map of Community Groups in Seattle</a></h1></div>
+      <div class="header-logo-container grid_4 alpha">
+        <div class="logo grid_6 alpha"><h1><a href="/"><img typeof="foaf:Image" src="http://datawiki.local/sites/all/themes/datawiki/images/city-groups-logo.png" alt="CityGroups" /></a></h1></div>
+      </div>
       <div class="grid_6 omega">
         <ul class="menu">
           <li><a href="#popular-searches" id="popular-search">Popular Searches</a></li>
@@ -30,10 +35,17 @@
     <div id="page-content" class="grid_12"> 
       <div id="sidebar" class="grid_2 alpha">                 
         <div id="popular-terms" class="block">
+                  <h4>Popular Searches</h4>
           <ul>
             <li><a href="#block-watch">Block Watch</a></li>
             <li><a href="#online-newspaper">Online Newspaper</a></li>
           </ul>
+        </div>
+  
+        <div id="search-map" class="block">
+          <h4>Search the map</h4>
+          <input type="text" id="search-map-input" name="search-map" value="" size="20" maxlength="64" class="form-text" placeholder="Enter your address">
+          <input type="submit" id="submit-search-map" name="op" value="Go" class="form-submit">
         </div>
         
         <div id="block-add-groups" class="block">
@@ -48,7 +60,7 @@
         <div id="data"></div>
       </div>
 
-      <div id="popup-content">
+      <div id="popup-content" class="block">
           <div class="content">Click map to see where the groups are</div>
       </div>
     </div>                
