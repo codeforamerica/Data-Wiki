88f1f252624939cd5e9d0a3921402b042d94224f
Chach Sikes <chachasikes@gmail.com>
2011-06-14 09:15:37 -0700

    Added some blocks to map display page.

--- sites/all/modules/custom/datawiki/datawiki_group_map/includes/map.html.inc.php
+++ sites/all/modules/custom/datawiki/datawiki_group_map/includes/map.html.inc.php
@@ -6,7 +6,7 @@
 <html>
 <head>
   <meta charset="utf-8">
-  <title>SiteName</title>
+  <title>CityGroups: Map of Community Groups in Seattle</title>
   <link rel="stylesheet" href="<?php print $html_root; ?>/includes/style/fluid_grid.css" type="text/css" />
   <link rel="stylesheet" href="<?php print $html_root; ?>/includes/style/boilerplate.css" type="text/css" />
   <link rel="stylesheet" href="<?php print $html_root; ?>/includes/js/leaflet/leaflet.css" />
@@ -27,27 +27,35 @@
         </ul>
       </div> 
     </div>
-    <div id="page-content" class="grid_12">              
-        <div id="popular-terms" class="grid_4 alpha">
+    <div id="page-content" class="grid_12"> 
+      <div id="sidebar" class="grid_2 alpha">                 
+        <div id="popular-terms" class="block">
           <ul>
             <li><a href="#block-watch">Block Watch</a></li>
             <li><a href="#online-newspaper">Online Newspaper</a></li>
           </ul>
         </div>
         
-        <div id="map-area" class="grid_8 omega">
-          <div id="map" style="height: 600px"></div>
-          <div id="data"></div>
+        <div id="block-add-groups" class="block">
+          <ul>
+            <li><a href="/node-add-community-group">Add a Group</a></li>
+          </ul>
         </div>
 
-        <div id="popup-content">
-            <div class="content">Click map to see where the groups are</div>
-        </div>
-      </div>                
+      </div>     
+      <div id="map-area" class="grid_9 omega">
+        <div id="map" style="height: 600px"></div>
+        <div id="data"></div>
+      </div>
+
+      <div id="popup-content">
+          <div class="content">Click map to see where the groups are</div>
+      </div>
+    </div>                
     <div id="footer" class="grid_12">
       <ul class="menu">
         <li><a href="about">About</a></li>
-        <li><a href="groups">View as list</a></li>
+        <li><a href="groups">View all as list</a></li>
       </ul>
     </div>
   </div>
