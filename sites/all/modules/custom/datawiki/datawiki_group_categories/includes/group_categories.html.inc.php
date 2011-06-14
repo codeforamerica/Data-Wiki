3c4a00a0a377389cbaea1a4b8793181ff5b3510c
Chach Sikes <chachasikes@gmail.com>
2011-06-14 10:59:55 -0700

    Added topics module.

--- sites/all/modules/custom/datawiki/datawiki_group_categories/includes/group_categories.html.inc.php
+++ sites/all/modules/custom/datawiki/datawiki_group_categories/includes/group_categories.html.inc.php
@@ -1,23 +1,20 @@
 <?php
-  $html_root = drupal_get_path('module', 'datawiki_group_map');
+  $html_root = drupal_get_path('module', 'datawiki_group_categories');
 ?>
 
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
 <head>
   <meta charset="utf-8">
-  <title>CityGroups: Map of Community Groups in Seattle</title>
+  <title>CityGroups: Categories of Community Groups in Seattle</title>
   <link rel="stylesheet" href="<?php print $html_root; ?>/includes/style/fluid_grid.css" type="text/css" />
   <link rel="stylesheet" href="<?php print $html_root; ?>/includes/style/boilerplate.css" type="text/css" />
-  <link rel="stylesheet" href="<?php print $html_root; ?>/includes/js/leaflet/leaflet.css" />
   <link rel="stylesheet" href="sites/all/themes/datawiki/css/style.css"  />
 
   
   <link rel="stylesheet" href="<?php print $html_root; ?>/includes/style/style.css" type="text/css" />
-<!--[if lte IE 8]><link rel="stylesheet" href="<?php print $html_root; ?>/includes/style/leaflet/leaflet.ie.css" /><![endif]-->
   <script type="text/javascript" src="<?php print $html_root; ?>/includes/js/jquery-1.5.min.js"></script>
-  <script type="text/javascript" src="<?php print $html_root; ?>/includes/js/leaflet/leaflet.js"></script>
-  <script type="text/javascript" src="<?php print $html_root; ?>/includes/js/map.js"></script>
+  <script type="text/javascript" src="<?php print $html_root; ?>/includes/js/topics.js"></script>
 </head>
 <body>
 <div id="container" class="section container_12">
@@ -33,26 +30,23 @@
       </div> 
     </div>
     <div id="page-content" class="grid_12"> 
-      <h3>Map</h3>
+      <h3>Categories</h3>
       <div id="tagline">A public directory of groups in Seattle.</div>
+<!--
     
       <div id="sidebar" class="grid_2 alpha">                 
-        <div id="popular-terms" class="block">
-                  <h4>Popular Searches</h4>
-          <ul>
-            <li><a href="#defaultPath">See All</a></li>
-            <li><a href="#block-watch">Block Watch</a></li>
-            <li><a href="#online-newspaper">Online Newspaper</a></li>
-            <li><a href="groups/categories">All Categories</a></li>
-          </ul>
-        </div>
+      </div>
+-->
   
+<!--
         <div id="search-map" class="block">
           <h4>Search the map</h4>
           <input type="text" id="search-map-input" name="search-map" value="" size="20" maxlength="64" class="form-text" placeholder="Enter your address">
           <input type="submit" id="search-map-submit" name="op" value="Go" class="form-submit">
         </div>
         
+-->
+<!--
         <div id="block-add-groups" class="block">
           <h4>Get Listed</h4>
           <p></p>
@@ -60,16 +54,13 @@
             <li><a href="node/add/community-group">Add a Group</a></li>
           </ul>
         </div>
+-->
 
       </div>     
-      <div id="map-area" class="grid_9 omega">
-        <div id="map" style="height: 600px"></div>
-        <div id="data"></div>
+      <div id="topics-area" class="grid_9 omega">
+        <div id="data">TOPICS</div>
       </div>
 
-      <div id="popup-content" class="block">
-          <div class="content">Click map to see where the groups are</div>
-      </div>
     </div>                
     <div id="footer" class="grid_12">
       <ul class="menu">
--- sites/all/modules/custom/datawiki/datawiki_group_map/includes/js/map.js
+++ sites/all/modules/custom/datawiki/datawiki_group_map/includes/js/map.js
@@ -4,10 +4,12 @@ cityGroups.geoJSON = {};
 cityGroups.map.settings = {};
 cityGroups.map.rendered;
 cityGroups.data = {};
+cityGroups.map.form = {};
 
 // Custom search paths.
 cityGroups.paths = {
     "defaultPath": "community-groups-data",
+    "#defaultPath": "community-groups-data",
     "#block-watch": "community-group-data/term/block-watch",
     "#online-newspaper": "community-group-data/term/online-newspaper"
 };
@@ -22,10 +24,13 @@ $(document).ready(function() {
   cityGroups.data.popularLoad();
   cityGroups.loadData(cityGroups.paths['defaultPath']);
   cityGroups.mapPageInteractions();
+
 });
 
 
 cityGroups.mapPageInteractions = function () {
+  cityGroups.map.geocodeAddress();
+  
   $('ul.menu a#popular-search').click(function() {
   console.log("cl");
     $('div#popular-terms').css('backgroundColor', '#bb4433');
@@ -150,6 +155,10 @@ cityGroups.map.popupTemplate = function(node) {
   if(node.categories !== undefined) {
     output += '<div class="categories">' + node.categories + '</div>';
   }
+  
+/*   output += "sign in to edit"; */
+  output += '<a href="node/' + node.citygroups_nid + '"  >more</a> ';
+  output += '<a href="node/' + node.citygroups_nid + '/edit" >edit</a>';
   output += '</div>';
   return output;
 };
@@ -167,4 +176,126 @@ cityGroups.map.loadMap = function() {
   cityGroups.map.cloudmadeUrl = 'http://{s}.tile.cloudmade.com/b59bc8b09cd84af58fcef3019d84e662/997/256/{z}/{x}/{y}.png',
   cityGroups.map.cloudmade = new L.TileLayer(cityGroups.map.cloudmadeUrl, {maxZoom: 18});
   cityGroups.map.rendered.setView(cityGroups.map.settings.center, cityGroups.map.settings.zoom).addLayer(cityGroups.map.cloudmade);
+};
+
+/**
+ * Use Google's Geocode API to return a geocoded address.
+ */
+cityGroups.map.geocodeAddress = function (){
+  //http://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&sensor=true_or_false
+  $('div#search-map input#search-map-submit').click(function() {
+    var inputValue = $('input#search-map-input').val();
+
+    inputValue = cityGroups.spaceToPlus(inputValue);
+    var googleGeocode = 'http://maps.googleapis.com/maps/api/geocode/json?address=';
+    var geocodeRequest = googleGeocode + inputValue;
+    cityGroups.map.loadGeocodeSuccess();
+    
+    $.ajax({
+      url: geocodeRequest,
+      dataType: 'json',
+      data: data,
+      success: cityGroups.map.loadGeocodeSuccess,
+      error: cityGroups.loadDataError
+    });    
+    
+    console.log(inputValue);
+  });
+
+};
+
+/**
+ * Convert spaces in a string to pluses.
+ */
+cityGroups.spaceToPlus = function(val) {
+  // @TODO convert spaces to +
+  val = val;
+  console.log(val);
+  return val;
+};
+
+cityGroups.map.loadGeocodeSuccess = function(/* data */) {
+/*   console.log(data); */
+  // http://code.google.com/apis/maps/documentation/geocoding/
+
+  var testGeoCodedData =
+    {
+    "status": "OK",
+    "results": [ {
+      "types": [ "street_address" ],
+      "formatted_address": "1600 Amphitheatre Pkwy, Mountain View, CA 94043, USA",
+      "address_components": [ {
+        "long_name": "1600",
+        "short_name": "1600",
+        "types": [ "street_number" ]
+      }, {
+        "long_name": "Amphitheatre Pkwy",
+        "short_name": "Amphitheatre Pkwy",
+        "types": [ "route" ]
+      }, {
+        "long_name": "Mountain View",
+        "short_name": "Mountain View",
+        "types": [ "locality", "political" ]
+      }, {
+        "long_name": "California",
+        "short_name": "CA",
+        "types": [ "administrative_area_level_1", "political" ]
+      }, {
+        "long_name": "United States",
+        "short_name": "US",
+        "types": [ "country", "political" ]
+      }, {
+        "long_name": "94043",
+        "short_name": "94043",
+        "types": [ "postal_code" ]
+      } ],
+      "geometry": {
+        "location": {
+          "lat": 37.4219720,
+          "lng": -122.0841430
+        },
+        "location_type": "ROOFTOP",
+        "viewport": {
+          "southwest": {
+            "lat": 37.4188244,
+            "lng": -122.0872906
+          },
+          "northeast": {
+            "lat": 37.4251196,
+            "lng": -122.0809954
+          }
+        }
+      }
+    } ]
+  };
+    
+/*   cityGroups.map.form.mapGeocodedData(testGeoCodedData); */
+};
+
+cityGroups.map.form.mapGeocodedData = function(geocodedData) {
+  console.log(geocodedData);
+  var formObject = {};
+  formObject.field_address = "";
+/*
+  for (i in geocodedData.results.address_components) {
+    switch(geocodedData.results.address_components[i]["types"][0]) {
+      case 'street_number':
+        formObject.field_address += geocodedData.results.address_components[i]["long_name"] + " ";
+        break;
+      case 'route':
+        formObject.field_address += geocodedData.results.address_components[i]["long_name"];
+        break;
+      case 'locality':
+        formObject.field_city = geocodedData.results.address_components[i]["long_name"];
+        break;  
+      case 'administrative_area_level_1':
+        formObject.field_state = geocodedData.results.address_components[i]["short_name"];
+        break;  
+      case 'postal_code':
+        formObject.field_zipcode = geocodedData.results.address_components[i]["long_name"];
+        break;  
+    }
+  }
+*/
+console.log(formObject);
 };
