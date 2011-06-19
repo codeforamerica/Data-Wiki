063352cf235168f1354b9650fd5215ed901664a5
Chach Sikes <chachasikes@gmail.com>
2011-06-19 16:35:37 -0700

    Cleaning out excessive overridden templates in themes.

--- sites/all/themes/datawiki/templates/page.tpl.php
+++ sites/all/themes/datawiki/templates/page.tpl.php
@@ -4,7 +4,6 @@
  * Default theme implementation to display a single Drupal page.
  */
 ?>
-
 <div id="container" class="container_16">
     <?php if ($page['header_top']): ?>
     <div class="header-top grid_16">
@@ -15,23 +14,28 @@
     <div id="header" class="grid_16">
       <div class="header-logo-container grid_4 alpha">
         <?php print render($page['header_logo']); ?>
-        <?php print $sitename; ?>
+        <?php print $logo; ?>
+        <?php print $prealpha; ?>
       </div>
-      
+      <?php if($section['section_name']): ?>
+      <div class="header-subsection-container grid_4 alpha">
+        <?php print render($section['section_name']); ?>
+      </div>
+      <?php endif; ?>
       <div class="header-menu-container grid_2">
         <?php print render($page['header_menu']); ?>
       </div>
   
-      <div class="header-signin-container grid_4">
+      <div class="header-signin-container grid_2">
         <?php print render($page['header_signin']); ?>
         <?php print $account_link; ?>
       </div>
   
-      <div class="header-gap-container grid_6 omega">
+      <div class="header-gap-container grid_4 omega">
         <?php print render($page['header_gap']); ?>
       </div>
     </div>
-    <div id="tagline">
+    <div id="slogan">
       <?php print $site_slogan; ?>
     </div>
     <div class="clear"></div>
@@ -42,16 +46,15 @@
     </div>
     <?php endif; ?>
   <div class="clear"></div>
-  <div id="main" role="main" class="grid_16 alpha">
-    <div class="preface-container">
-      <?php print render($page['preface']); ?>
-    </div>
-    <div class="clear"></div>
+  <div id="main" role="main" class="grid_16 alpha omega">
     <div class="messages-container">
       <?php print $messages; ?>
     </div>
     <div class="clear"></div>
-    
+    <div class="preface-container grid_8 push_8 alpha omega">
+      <?php print render($page['preface']); ?>
+    </div>
+    <div class="clear"></div>
     <div class="title-container">
       <?php print render($title_prefix); ?>
       <?php if ($title): ?>
@@ -60,55 +63,56 @@
       <?php print render($title_suffix); ?>
     </div>
     <div class="clear"></div> 
-
-    <div class="main-content-region <?php print $main_region_width; ?> alpha">
-      <div class="tabs-container">
-        <?php print render($tabs); ?>
-      </div>
-      <div class="clear"></div>      
-      <div class="help-container">
-        <?php print render($page['help']); ?>
-      </div>
-      <div class="clear"></div>       
-      <div class="action-links-container">
-        <?php if ($action_links): ?>
-          <ul class="action-links"><?php print render($action_links); ?></ul>
-        <?php endif; ?>
-      </div>
-
-      <div class="clear"></div>
-      <div id="content-container">
-        <?php if ($page['content_top']): ?>
-          <div id="content-content-top" class="content-content-top">
-            <?php print render($page['content_top']); ?>
-          </div>
-        <?php endif; ?>
-        
-        <div class="clear"></div>                      
-        <?php if ($page['content']): ?>
-          <div id="content-content" class="content-content">
-            <?php print render($page['content']); ?>
-          </div>
-        <?php endif; ?>
-  
-        <div class="clear"></div>                      
-        <?php if ($page['content_bottom']): ?>
-          <div id="content-content-bottom" class="content-content-bottom">
-            <?php print render($page['content_bottom']); ?>
-          </div>
-        <?php endif; ?>
-      </div>
-    </div>
-
-    <div class="sidebar-container grid_4 push_1 omega">
-      <div class="sidebar-first-container">
+    <div id="main-content-container">
+      <?php if(!empty($page['sidebar_first'])): ?>
+      <div class="sidebar-first-container grid_5 omega">
         <?php print render($page['sidebar_first']); ?>
       </div>
+      <?php endif; ?>
+      <div class="main-content-region <?php print $main_region_width; ?> alpha">
+        <div class="tabs-container">
+          <?php print render($tabs); ?>
+        </div>
+        <div class="clear"></div>      
+        <div class="help-container">
+          <?php print render($page['help']); ?>
+        </div>
+        <div class="clear"></div>       
+        <div class="action-links-container">
+          <?php if ($action_links): ?>
+            <ul class="action-links"><?php print render($action_links); ?></ul>
+          <?php endif; ?>
+        </div>
   
-      <div class="sidebar-second-container">
-        <?php print render($page['sidebar_second']); ?>
+        <div class="clear"></div>
+        <div id="content-container">
+          <?php if ($page['content_top']): ?>
+            <div id="content-content-top" class="content-content-top">
+              <?php print render($page['content_top']); ?>
+            </div>
+          <?php endif; ?>
+          
+          <div class="clear"></div>                      
+          <?php if ($page['content']): ?>
+            <div id="content-content" class="content-content">
+              <?php print render($page['content']); ?>
+            </div>
+          <?php endif; ?>
+    
+          <div class="clear"></div>                      
+          <?php if ($page['content_bottom']): ?>
+            <div id="content-content-bottom" class="content-content-bottom">
+              <?php print render($page['content_bottom']); ?>
+            </div>
+          <?php endif; ?>
+        </div>
       </div>
+    </div> 
+    <?php if(!empty($page['sidebar_second'])): ?>
+    <div class="sidebar-second-container grid_5 omega">
+      <?php print render($page['sidebar_second']); ?>
     </div>
+    <?php endif; ?>
     <div class="clear"></div>
     <div class="postscript-container grid_16 alpha">
       <?php print render($page['postscript']); ?>
@@ -121,7 +125,7 @@
       <div class="clear"></div> 
       <?php print render($page['footer']); ?>
       <div class="clear"></div>
-      <?php print render($page['footer']); ?>
+      <?php print render($page['footer_bottom']); ?>
       <div class="clear"></div>
   </div>
   <div class="clear"></div>
