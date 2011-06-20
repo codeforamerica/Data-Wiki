f7f04dc1f159c0d8bb7aa6e7555b529d115c988c
Chach Sikes <chachasikes@gmail.com>
2011-06-19 22:43:39 -0700

    Removed extra templates.

--- sites/all/themes/datawiki/templates/html.tpl.php
+++ sites/all/themes/datawiki/templates/html.tpl.php
@@ -34,7 +34,7 @@
  */
 
 ?>
-<!doctype html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html class="no-js" xmlns:fb="http://www.facebook.com/2008/fbml" lang="<?php print $language->language; ?>">
 <head profile="<?php print $grddl_profile; ?>">
   <meta charset="utf-8">
