ec8d0c09249c71ce71fcea0ae07ff370ad93819b
Chach Sikes <chachasikes@gmail.com>
2011-06-21 16:37:22 -0700

    Updated map loading.

--- sites/all/modules/custom/datawiki/community_group_form/js/community_group_form.js
+++ sites/all/modules/custom/datawiki/community_group_form/js/community_group_form.js
@@ -5,9 +5,8 @@ Drupal.settings.community_group_form = {};
 
 Drupal.behaviors.community_group_form = {
   'attach': function(context, settings) {
-    Drupal.settings.community_group_form.data = $('div.openlayers-map').data('openlayers');
-    //Drupal.settings.community_group_form.openLayersDrawPoint();
-//     Drupal.settings.community_group_form.data.map.width = 400;
+      Drupal.settings.community_group_form.data = $('div.openlayers-map').data('openlayers');
+      Drupal.settings.community_group_form.centerOnFeature();
     }
   };
 
@@ -40,7 +39,6 @@ Drupal.behaviors.community_group_form = {
   for (var field in fields) {
     // Move the description markup to an interactive element.
     // @TODO a focus css might be a better approach in the long term.
-    console.log(field);
     description = $(field + ' div.description').html();
     label =  $(field + ' label').html();
     if(description !== null) {
@@ -50,7 +48,7 @@ Drupal.behaviors.community_group_form = {
       
       if(fields[field]){
         $(field + ' label').remove();
-        $(field + ' input').before('<label>' +label + '</label>');
+        $(field + ' input').before('<label>' + label + '</label>');
       }
 
     }
@@ -66,29 +64,31 @@ Drupal.behaviors.community_group_form = {
   });
 
   var geocodedAddressResults;
+  
+  // Autoload existing address.
+  $('div.map-instructions-container div.address-ajax input#search-map-input').val($('textarea#edit-field-address-und-0-value').val());
 
- $('div.map-instructions-container div.address-ajax div.submit').click(function () {
-   //Drupal.settings.community_group_form.geocodeAddress();
-   return false;
+
+
+  
+  $('div.map-instructions-container div.address-ajax div.submit').click(function () {
+  Drupal.settings.community_group_form.geocodeAddress();
+  return false;
  });
 
 
 
 var geocoder;
-//geocoder = new google.maps.Geocoder();
+geocoder = new google.maps.Geocoder();
 
 Drupal.settings.community_group_form.geocodeAddress = function (){
-  //  var inputValue = $('div.map-instructions-container div.address-ajax input#search-map-input').val();
-    var inputValue = "2025 McGee Ave, Berkeley CA";
-    // var googleGeocode = 'http://maps.googleapis.com/maps/api/geocode/json?sensor=false&address=';
-    // var geocodeRequest = googleGeocode + inputValue;
-    var address = inputValue;
-    Drupal.settings.community_group_form.codeAddress(address);
+    var inputValue = $('div.map-instructions-container div.address-ajax input#search-map-input').val();
+    console.log(inputValue);
+    Drupal.settings.community_group_form.codeAddress(inputValue);
 };
 
-
 Drupal.settings.community_group_form.codeAddress = function(address) {
- var result =  geocoder.geocode( { 'address': address}, function(results, status) {
+  var result =  geocoder.geocode( { 'address': address}, function(results, status) {
     if (status == google.maps.GeocoderStatus.OK) {
        Drupal.settings.community_group_form.mapGeocodedData(results);
     } 
@@ -100,12 +100,11 @@ Drupal.settings.community_group_form.codeAddress = function(address) {
 }
 
 Drupal.settings.community_group_form.mapGeocodedData = function(results) {
-
   var result = {};
   result.field_address = '';
   console.log(results);
   var item = results[0]['address_components'];
-  //console.log(item);
+
   for (i in item) {     
     switch(item[i]["types"][0]) {
       case 'street_number':
@@ -130,11 +129,12 @@ Drupal.settings.community_group_form.mapGeocodedData = function(results) {
    result.longitude = results[0]["geometry"]["location"]["Ia"];  
   }
 
- // Drupal.settings.community_group_form.insertGeocodedFieldData(result);
- // Drupal.settings.community_group_form.openLayersDropPoint(result);
+  Drupal.settings.community_group_form.updateMapForm(result);
 };
     
-Drupal.settings.community_group_form.insertGeocodedFieldData = function(result) {
+Drupal.settings.community_group_form.updateMapForm = function(result) {
+  // @TODO use values from geofield instead (data_form)
+  
   if((result.field_address !== undefined) && (result.field_address !== '')) {
     $('textarea#edit-field-address-und-0-value').val(result.field_address);
   }
@@ -154,16 +154,50 @@ Drupal.settings.community_group_form.insertGeocodedFieldData = function(result) 
     $('input#geofield_lon').val(result.longitude);
   } 
   
+  Drupal.settings.community_group_form.openLayersDropPoint(result);
 };
 
 Drupal.settings.community_group_form.openLayersDropPoint = function(result){
   // Get map data.
   var data = Drupal.settings.community_group_form.data;
-  
-  if(result.longitude !== undefined && result.latitude !== undefined) {
-    data.openlayers.setCenter(new OpenLayers.LonLat(parseFloat(result.longitude), parseFloat(result.latitude)), 3, false, false);
-    //data.openlayers.setCenter(new OpenLayers.LonLat(-122,  37), 3, false, false);
+
+  if(result.longitude !== undefined && result.latitude !== undefined) {    
+    var center = new OpenLayers.LonLat(result.longitude, result.latitude).transform(
+            new OpenLayers.Projection('EPSG:4326'),
+            new OpenLayers.Projection(data.openlayers.projection.projCode));
+    var zoomBlockLevel = 15;
+    var zoomMultiBlock = 13;
+    data.openlayers.setCenter(center, zoomBlockLevel, false, false);
   }
+};
+
+Drupal.settings.community_group_form.centerOnFeature = function() {
+  var data = Drupal.settings.community_group_form.data;
+
+
+  // On load, center on the feature and zoom.
+  // @TODO this is ignoring the openlayers preset for some reason. 
+  var zoomBlockLevel = 15;
+  var zoomMultiBlock = 13;
+  
+/*
+  var lat = $('input#geofield_lat').val();
+  var lon = $('input#geofield_lat').val();
+  
+  console.log(lat);
+*/
+
+  var  geom = feature.clone().geometry.transform(
+  feature.layer.map.projection,
+  new OpenLayers.Projection('EPSG:4326'));
+
+  centroid = geom.getCentroid();
+
+  var center = new OpenLayers.LonLat(centroid.x, centroid.y).transform(
+          new OpenLayers.Projection('EPSG:4326'),
+          new OpenLayers.Projection(data.openlayers.projection.projCode));
+  data.openlayers.setCenter(center, zoomBlockLevel, false, false);
+        
 };
 
 })(jQuery);
