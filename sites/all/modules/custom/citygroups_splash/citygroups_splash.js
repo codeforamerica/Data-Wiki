f75e9356c3dacfe75a53ac75fbcdb9a583935675
Chach Sikes <chachasikes@gmail.com>
2011-06-28 17:54:47 -0700

    Set message for pre-alpha screen.

--- sites/all/modules/custom/citygroups_splash/citygroups_splash.js
+++ sites/all/modules/custom/citygroups_splash/citygroups_splash.js
@@ -5,6 +5,8 @@
       $('.citygroups-splash-link', context).click(function() {
         setTimeout($.colorbox.close, 2000);
       });
+      // Set cookie that the user has already seen the prealpha message.
+      document.cookie = 'prealpha_seen=1';
     }
   };
 })(jQuery);
