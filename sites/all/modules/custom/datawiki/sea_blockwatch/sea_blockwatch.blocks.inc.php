af71ba60f37ef8e568a0009518f9b49acc9d6b87
Chach Sikes <chachasikes@gmail.com>
2011-08-26 20:01:16 -0700

    Added custom classes to menus.

--- sites/all/modules/custom/datawiki/sea_blockwatch/sea_blockwatch.blocks.inc.php
+++ sites/all/modules/custom/datawiki/sea_blockwatch/sea_blockwatch.blocks.inc.php
@@ -120,22 +120,22 @@ function sea_blockwatch_contents($type) {
         $output .= "
         <div class=\"grid_16 postscript-content alpha omega\">
           <div class=\"grid_6 about alpha\">
-          <h3>What is BWCF</h3>
-          
-          We make it easy to find and share all kinds of groups in your city
-          <div class=\"groups-icons\">" . $links . "</div>
-          
-          <p>Banh mi vinyl vegan laboris magna, nihil hoodie portland homo cillum raw denim tempor. High life wayfarers you probably haven't heard of them, assumenda twee wes anderson ut dreamcatcher put a bird on it nulla echo park marfa master cleanse consequat quis. Dolor adipisicing accusamus hoodie chambray blog. Qui ullamco laboris whatever shoreditch voluptate. Brooklyn sartorial cliche, dolore whatever exercitation odio scenester. Chambray scenester esse magna locavore. Stumptown proident pariatur nesciunt.</p>
+            <h3>What is a Block Watch Captain?</h3>
+            <p>Block Watch is a national program that is based on the principle that neighbors working together are the first and best line of defense against crime.</p>
+            <p>Block Watch Captains take the lead among their neighbors by partnering with the police department and other city departments to alert them to suspicious activity and creating a better sense of community. Block parties, street cleanups and other creative initiatives at the block or neighborhood level makes this work</p>
           </div>
   
           <div class=\"grid_4 push_1 participate\">
           <h3>Get on the map</h3>
-          <p>Banh mi vinyl vegan laboris magna, nihil hoodie portland homo cillum raw denim tempor. High life wayfarers you probably haven't heard of them, assumenda twee wes anderson ut dreamcatcher put a bird on it nulla echo park marfa master cleanse consequat quis. Dolor adipisicing accusamus hoodie chambray blog. Qui ullamco laboris whatever shoreditch voluptate. Brooklyn sartorial cliche, dolore whatever exercitation odio scenester. Chambray scenester esse magna locavore. Stumptown proident pariatur nesciunt.</p>
+            <p>The Seattle Police Department estimates that there may be over 4,000 Block Watch Captains in various Block Watch Communities throughout Seattle.</p>
+            <p>If you want to help your neighborhood get on the map, contact XXXXX, a community of Block Watch Organizers & Emergency Preparedness community leaders who are working to make Seattle safer by building community.</p>         
           </div>
-    
+
           <div class=\"grid_4 push_1 local omega\">
-          <h3>Local Community Groups</h3>
-          <p>Banh mi vinyl vegan laboris magna, nihil hoodie portland homo cillum raw denim tempor. High life wayfarers you probably haven't heard of them, assumenda twee wes anderson ut dreamcatcher put a bird on it nulla echo park marfa master cleanse consequat quis. Dolor adipisicing accusamus hoodie chambray blog. Qui ullamco laboris whatever shoreditch voluptate. Brooklyn sartorial cliche, dolore whatever exercitation odio scenester. Chambray scenester esse magna locavore. Stumptown proident pariatur nesciunt.</p>
+            <h3>Block Watch Resources</h3>
+            <p>Get connected to the Seattle Police Department and other local crime prevention & emergency preparedness initiatives.</p>
+            <h3>More Local community groups</h3>
+            <p>CityGroups lists other kinds of community groups working to make your neighborhood better in Seattle. Find other community leaders near you.</p>
           </div>
         </div>        
         ";
