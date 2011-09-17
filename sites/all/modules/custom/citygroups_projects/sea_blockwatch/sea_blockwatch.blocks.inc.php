ec5e62553f2240df3b8a9fd7057485d38b270f91
Chach Sikes <chachasikes@gmail.com>
2011-09-16 21:39:13 -0700

    Set block watch about content in form.

--- sites/all/modules/custom/citygroups_projects/sea_blockwatch/sea_blockwatch.blocks.inc.php
+++ sites/all/modules/custom/citygroups_projects/sea_blockwatch/sea_blockwatch.blocks.inc.php
@@ -52,7 +52,8 @@ function sea_blockwatch_contents($type) {
     
   switch ($type) {
     case 'welcome':
-      $output .= '<p>' . t('The Block Watch Captain Directory lists basic contact information for Block Watch Captains in Seattle.') .'</p>';
+      $output .= '<h4>' . variable_get('sea_blockwatch_slogan') .'</h4>';
+      $output .= '<p>' . variable_get('sea_blockwatch_content_tagline') .'</p>';
       break;
       
       case 'home_content':
@@ -102,31 +103,21 @@ function sea_blockwatch_contents($type) {
   
         $links = $facebook . $google . $meetup . $mailinglist . $twitter . $blog . $yahoo . $mail /*. $ning */;
 
-        $output .= "
-        <div class=\"grid_16 postscript-content alpha omega\">
-          <div class=\"grid_6 about alpha\">
-            <h3>What is a Block Watch Captain?</h3>
-              <p>Block Watch is a national program that is based on the principle that neighbors working together are the first and best line of defense against crime. The Captain is also the logical person to organize the efforts to prepare for a disaster or emergency like a major earthquake.  Each household and every block needs to be prepared.</p>
-              <p>Block Watch Captains take the lead among their neighbors by partnering with the Seattle Police Department, the Department of Emergency Preparedness, and other city departments to alert them to suspicious activity,  to organize their block to prepare for and respond to an emergency, and create a better sense of community as neighbors help each other. Block parties, emergency kit building, street cleanups and other creative initiatives at the block or neighborhood level makes this work.</p>
-          </div>
+        $output .= '
+        <div class="grid_16 postscript-content alpha omega">
+          <div class="grid_6 about alpha">'
+          . variable_get('sea_blockwatch_content_about') .
+          '</div>
   
-          <div class=\"grid_4 push_1 participate\">
-          <h3>Get on the map</h3>
-            <p>The Seattle Police Department estimates that there may be over 4,000 Block Watch Captains in various Block Watch Communities throughout Seattle.</p>
-            <p>If you want to help your neighborhood get on the map, contact XXXXX, a community of Block Watch Organizers & Emergency Preparedness community leaders who are working to make Seattle safer by building community.</p>         
-          </div>
+          <div class="grid_4 push_1 participate">'
+          . variable_get('sea_blockwatch_content_about_participate') .
+          '</div>
 
-          <div class=\"grid_4 push_1 local omega\">
-            <h3>Block Watch Resources</h3>
-            <p>Get connected to the Seattle Police Department and other local crime prevention & emergency preparedness initiatives.</p>
-            <p><a href=\"/blockwatch/resources\">See list of resources</a></p>
-            <br />
-            <h3>More Local community groups</h3>
-            <p>CityGroups lists other kinds of community groups working to make your neighborhood better in Seattle. Find other community leaders near you.</p>
-            <p><a href=\"/blockwatch/categories\">See related groups</a></p>
-          </div>
+          <div class="grid_4 push_1 local omega">'
+          . variable_get('sea_blockwatch_content_about_local') .
+          '</div>
         </div>        
-        ";
+        ';
       break;      
 
   }
